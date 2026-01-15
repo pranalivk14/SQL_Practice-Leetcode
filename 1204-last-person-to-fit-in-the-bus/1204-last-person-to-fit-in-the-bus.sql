fdf7bdd312cd5a4b4d70dc7473bc870054dc1f93
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-15 02:05:39 -0800

    Time: 929 ms (30.74%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,8 +1,9 @@
 SELECT person_name
-FROM ( SELECT person_name,
-        SUM(weight) OVER (ORDER BY turn) AS total_weight,
-        turn 
-    FROM Queue) AS cte
+FROM
+    (SELECT person_name, 
+            SUM(weight) OVER (ORDER BY turn) AS total_weight,
+            turn
+    FROM Queue) cte
 WHERE total_weight <= 1000
 ORDER BY turn DESC
 LIMIT 1
