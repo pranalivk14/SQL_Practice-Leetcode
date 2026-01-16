b8212294509a707f5bbea7e9f4f39de5b3904c41
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-16 01:17:23 -0800

    Time: 396 ms (10.83%), Space: 0B (100%) - LeetHub

---
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -0,0 +1,7 @@
+SELECT (CASE WHEN MOD(id,2)!=1 THEN id-1
+            WHEN MOD(id,2)=1 AND id IN (SELECT MAX(id) FROM Seat) THEN id
+            ELSE id+1
+            END) id,
+        student
+FROM Seat
+ORDER BY id
