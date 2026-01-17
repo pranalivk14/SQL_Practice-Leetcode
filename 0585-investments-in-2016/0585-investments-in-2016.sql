46563ddf333c98fbd462a811a3f0ee8e2521c908
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-16 16:08:02 -0800

    Time: 601 ms (21.96%), Space: 0B (100%) - LeetHub

--- 0585-investments-in-2016/0585-investments-in-2016.sql
+++ 0585-investments-in-2016/0585-investments-in-2016.sql
@@ -1,15 +1,12 @@
-SELECT ROUND(SUM(temp1.tiv_2016),2) AS tiv_2016
-FROM 
-
-(SELECT tiv_2016, lat, lon 
+SELECT ROUND(SUM(tiv_2016),2) AS tiv_2016
 FROM Insurance
-GROUP BY lat, lon 
-HAVING COUNT(*)=1) temp1
-
-JOIN
-
-(SELECT DISTINCT i1.tiv_2016
-FROM Insurance i1 JOIN Insurance i2 ON i1.tiv_2015=i2.tiv_2015
-WHERE i1.tiv_2015=i2.tiv_2015 AND i1.pid<>i2.pid) temp2
-
-ON temp1.tiv_2016=temp2.tiv_2016
+WHERE tiv_2015 IN
+    (SELECT tiv_2015
+    FROM Insurance
+    GROUP BY tiv_2015 
+    HAVING COUNT(*)>1)
+AND (lat, lon) IN
+    (SELECT lat, lon
+    FROM Insurance
+    GROUP BY lat, lon
+    HAVING COUNT(*)=1)
