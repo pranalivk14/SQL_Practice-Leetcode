224742127814f0cdb521ac514c0b90deb6e0a6ba
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-18 14:11:44 -0800

    Time: 1236 ms (51.83%), Space: 0B (100%) - LeetHub

--- 1341-movie-rating/1341-movie-rating.sql
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -1,20 +1,18 @@
-SELECT results
+SELECT results 
 FROM
-    (SELECT name AS results, COUNT(rating) AS count_rating
-    FROM Users u JOIN MovieRating mr ON u.user_id=mr.user_id
-    GROUP BY name
-    ORDER BY count_rating DESC, name ASC
-    LIMIT 1
-    ) AS temp1
-UNION ALL
-SELECT results
-FROM
-    (
-    SELECT m.title AS results, AVG(rating) AS avg_rating
-    FROM Movies m JOIN MovieRating mr ON m.movie_id=mr.movie_id
-    WHERE DATE_FORMAT(created_at, '%m-%Y') = '02-2020'
-    GROUP BY m.title
-    ORDER BY avg_rating DESC, m.title ASC
-    LIMIT 1
-    ) AS temp2
+(SELECT u.name AS results
+FROM Users u JOIN MovieRating mr ON u.user_id=mr.user_id
+GROUP BY u.user_id
+ORDER BY COUNT(mr.rating) DESC, u.name ASC
+LIMIT 1)AS table1
 
+UNION ALL
+
+SELECT results 
+FROM
+(SELECT m.title AS results
+FROM Movies m JOIN MovieRating mr ON m.movie_id=mr.movie_id
+WHERE DATE_FORMAT(created_at, '%m-%y')= '02-20'
+GROUP BY m.movie_id
+ORDER BY AVG(mr.rating) DESC, m.title ASC
+LIMIT 1) AS table2
