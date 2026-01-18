b5bc4cdd9522fd250f0c648548eea4dc7832380c
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-17 16:08:34 -0800

    Time: 600 ms (57.03%), Space: 0B (100%) - LeetHub

--- 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
+++ 1890-the-latest-login-in-2020/1890-the-latest-login-in-2020.sql
@@ -1,4 +1,7 @@
 SELECT user_id, MAX(time_stamp) AS last_stamp
-FROM Logins
-WHERE DATE_FORMAT(time_stamp, '%Y')='2020'
+FROM
+    (SELECT * 
+    FROM Logins
+    WHERE DATE_FORMAT(time_stamp, '%Y')=2020) AS temp
 GROUP BY user_id
+
