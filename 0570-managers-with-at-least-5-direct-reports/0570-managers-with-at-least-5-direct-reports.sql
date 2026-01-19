7b2b333cfc7ad2470eb45746808f417292ffb15f
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-18 16:39:35 -0800

    Time: 324 ms (69.53%), Space: 0B (100%) - LeetHub

--- 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ 0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,4 @@
 SELECT e2.name
-FROM Employee e1 JOIN Employee e2 ON e1.managerId = e2.id
+FROM Employee e1 JOIN Employee e2 ON e1.managerId=e2.id
 GROUP BY e1.managerId
-HAVING count(e1.managerId)>= 5
+HAVING COUNT(e1.managerId)>=5
