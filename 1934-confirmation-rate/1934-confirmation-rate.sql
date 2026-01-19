e3b8a7bb812905e75ebefc535cafe5bc90d875e7
Pranali Kulkarni <pranalivk14@gmail.com>
2026-01-18 16:09:39 -0800

    Time: 651 ms (37.32%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,10 +1,4 @@
-WITH CTE1 AS
-(
-SELECT *, count(user_id) AS total_users,
-SUM(CASE WHEN action = 'confirmed' THEN 1 ELSE 0 END) AS confirmed_users
-FROM Confirmations c 
-GROUP BY user_id
-)
-
-SELECT s.user_id, ROUND(IFNULL((CTE1.confirmed_users/CTE1.total_users), 0),2) AS confirmation_rate
-FROM Signups s LEFT JOIN CTE1 ON s.user_id = CTE1.user_id
+SELECT s.user_id, 
+    IFNULL(ROUND(SUM(CASE WHEN c.action='confirmed' THEN 1 ELSE 0 END)/COUNT(c.action),2),0) AS confirmation_rate
+FROM Signups s LEFT JOIN Confirmations c ON s.user_id=c.user_id
+GROUP BY s.user_id
